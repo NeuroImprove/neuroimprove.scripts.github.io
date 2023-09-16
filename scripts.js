c50ab23405dde17fdaae56391a4339be17bfb3b9
Jorge Perestrelo <65506551+jperestrelo@users.noreply.github.com>
2023-09-16 17:17:44 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,18 +1,19 @@
-var n = n || [];
-n.push(function() {
-  $(document).off("submit"), $("form").submit(function(s) {
-    s.preventDefault();
-    const t = $(this), o = $("[type=submit]", t), e = o.val(), i = o.attr("data-wait"), r = t.attr("method"), f = t.attr("action"), a = t.attr("data-redirect"), m = t.serialize();
+console.log("form settings loaded!");
+var s = s || [];
+s.push(function() {
+  $(document).off("submit"), $("form").submit(function(a) {
+    a.preventDefault();
+    const t = $(this), o = $("[type=submit]", t), e = o.val(), i = o.attr("data-wait"), r = t.attr("method"), f = t.attr("action"), n = t.attr("data-redirect"), d = t.serialize();
     i && o.val(i), $.ajax(f, {
-      data: m,
+      data: d,
       method: r
-    }).done((c) => {
-      if (a) {
-        window.location = a;
+    }).done((l) => {
+      if (n) {
+        window.location = n;
         return;
       }
       t.hide().siblings(".w-form-done").show().siblings(".w-form-fail").hide();
-    }).fail((c) => {
+    }).fail((l) => {
       t.siblings(".w-form-done").hide().siblings(".w-form-fail").show();
     }).always(() => {
       o.val(e);
