d03d28e1d00d8c74fdaf5505ad043ed06e272b95
Jorge Perestrelo <65506551+jperestrelo@users.noreply.github.com>
2023-09-16 18:09:00 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -40,10 +40,12 @@ window.addEventListener("DOMContentLoaded", () => {
   if (Object.keys(utmParams).length > 0) {
     setCookie("utm_params", JSON.stringify(utmParams), 30);
   }
-  const utmParamsFromCookies = JSON.parse(getCookie("utm_params"));
+  const utmParamsFromCookies = getCookie("utm_params") ? JSON.parse(getCookie("utm_params")) : {};
   if (utmParamsFromCookies) {
     const pageTitle = document.title;
     utmParamsFromCookies["landing_page"] = pageTitle;
+    const hostName = window.location.hostname;
+    utmParamsFromCookies["hostname"] = hostName;
     const forms = document.querySelectorAll('form[formType="captureLead"]');
     forms.forEach((form) => {
       Object.entries(utmParamsFromCookies).forEach(([key, value]) => {
